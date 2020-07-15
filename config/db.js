f06a5152ec78f79edd710a16937d7b1ef38d32ed
Anant Kumar Iyer <42653125+CobbleDesigns@users.noreply.github.com>
2020-07-15 18:42:27 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const keys = require('./keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose
+    .connect(keys.mongoURI, { useNewUrlParser: true,
+        useUnifiedTopology: true }
+    )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://reverseFlash:passw0rd@test-cluster-5bjel.mongodb.net/users?retryWrites=true&w=majority',
+    pusherAppId: '1027548',
+    pusherKey: '53e5a95be4000aad6b6b',
+    pusherSecret: '708f6d22635af3dbe434',
+    pusherCluster: 'ap2',
+    pusherEncrypted: true
+  };
+  
